1f812df766ded616389831e80cade18aa46cba52
Osiron007 <nieland@neobotix.de>
2019-03-18 13:15:45 +0100

    fixed vel for obstacles and file names

--- scripts/spawn_obstacles_track2.py
+++ scripts/spawn_obstacles_track2.py
@@ -11,7 +11,7 @@ from pathlib import Path
 
 
 home = expanduser("~")
-path = home + '/.gazebo/models/construction_cone/model.sdf'
+path = home + '/.gazebo/models/construction_barrel/model.sdf'
 
 class Moving():
 	def __init__(self, model_name, Spawning1, y_pose, x_pose):
@@ -38,11 +38,11 @@ class Moving():
 		model = rospy.wait_for_message('gazebo/model_states', ModelStates)
 		model_original_pose_y = model.pose[0].position.y
 		for i in range(len(model.name)):
-			if model.name[i] == self.model_name and round(model.pose[i].position.y) <= round(self.y_model_pose)+6.0 and self.flag == 0:
+			if model.name[i] == self.model_name and round(model.pose[i].position.y) <= round(self.y_model_pose)+5.0 and self.flag == 0:
 				obstacle.model_name = self.model_name
 				obstacle.pose = model.pose[i]
 				obstacle.twist = Twist()
-				obstacle.twist.linear.y = 1.4
+				obstacle.twist.linear.y = 1.3
 				obstacle.twist.angular.z = 0
 				self.pub_model.publish(obstacle)
 			elif model.name[i] == self.model_name and round(model.pose[i].position.y) != round(self.y_model_pose):
@@ -50,55 +50,53 @@ class Moving():
 				obstacle.model_name = self.model_name
 				obstacle.pose = model.pose[i]
 				obstacle.twist = Twist()
-				obstacle.twist.linear.y = -1.4
+				obstacle.twist.linear.y = -1.3
 				obstacle.twist.angular.z = 0
 				self.pub_model.publish(obstacle)
 			elif  round(model.pose[i].position.y) == round(self.y_model_pose):
 				self.flag = 0
 
-	def moving_tile_y_alternate_long_path(self):
+	def moving_tile_y_short_path(self):
 		obstacle = ModelState()
 		model = rospy.wait_for_message('gazebo/model_states', ModelStates)
 		model_original_pose_y = model.pose[0].position.y
 		for i in range(len(model.name)):
-			if model.name[i] == self.model_name and round(model.pose[i].position.y) <= round(self.y_model_pose)+6.0 and round(model.pose[i].position.y) >= -round(self.y_model_pose)+2. and self.flag1 == 0:
+			if model.name[i] == self.model_name and round((model.pose[i].position.y),1) <= round(self.y_model_pose,1)+1.0 and self.flag1 == 0:
 				obstacle.model_name = self.model_name
 				obstacle.pose = model.pose[i]
 				obstacle.twist = Twist()
-				obstacle.twist.linear.y = -1.4
+				obstacle.twist.linear.y = 0.5
 				obstacle.twist.angular.z = 0
 				self.pub_model.publish(obstacle)
-			elif model.name[i] == self.model_name and round(model.pose[i].position.y) != round(self.y_model_pose):
+			elif model.name[i] == self.model_name and round(model.pose[i].position.y,1) != round(self.y_model_pose,1):
 				self.flag1 = 1
 				obstacle.model_name = self.model_name
 				obstacle.pose = model.pose[i]
 				obstacle.twist = Twist()
-				obstacle.twist.linear.y = 1.4
+				obstacle.twist.linear.y = -0.5
 				obstacle.twist.angular.z = 0
 				self.pub_model.publish(obstacle)
-			elif  round(model.pose[i].position.y) == round(self.y_model_pose):
+			elif  round(model.pose[i].position.y,1) == round(self.y_model_pose,1):
 				self.flag1 = 0
 
 def main():
 	rospy.init_node('moving_obstacle')
+	rospy.loginfo("Here")
 	Spawning1 = rospy.ServiceProxy("gazebo/spawn_sdf_model", SpawnModel)
 	rospy.wait_for_service("gazebo/spawn_sdf_model")
-	moving = Moving("construction_barrel", Spawning1, -4, 1.0)
-	moving1 = Moving("construction_barrel_1", Spawning1, 4, -1.)
-	moving2 = Moving("construction_barrel_2", Spawning1, -4, -3.5)
-	moving3 = Moving("construction_barrel_3", Spawning1, 4, -6.)
-	moving4 = Moving("construction_barrel_4", Spawning1, 4, 3.5)
+	moving = Moving("construction_barrel", Spawning1, -17.13, 9.78)
+	moving1 = Moving("construction_barrel_1", Spawning1, -16.33, -13.9)
+	moving2 = Moving("construction_barrel_2", Spawning1, 0.1, -3.5)
+	moving3 = Moving("construction_barrel_3", Spawning1, -27.1, -4.8)
 	moving.spawning()
 	moving1.spawning()
 	moving2.spawning()
 	moving3.spawning()
-	moving4.spawning()
 	while not rospy.is_shutdown():
 		moving.moving_tile_y_long_path()
-		moving1.moving_tile_y_alternate_long_path()
-		moving2.moving_tile_y_long_path()
-		moving3.moving_tile_y_alternate_long_path()
-		moving4.moving_tile_y_alternate_long_path()
+		moving1.moving_tile_y_long_path()
+		moving2.moving_tile_y_short_path()
+		moving3.moving_tile_y_short_path()
 
 
 if __name__ == '__main__':
